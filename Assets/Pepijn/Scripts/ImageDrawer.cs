572d80b7083c60c39043c84ad90c9beaba4506e5
nuno <ruiduidahh@Gmail.com>
2024-06-19 01:46:49 +0200

    cursor paint werkend

--- Assets/Pepijn/Scripts/ImageDrawer.cs
+++ Assets/Pepijn/Scripts/ImageDrawer.cs
@@ -61,19 +61,10 @@ public class ImageDrawer : NetworkBehaviour
 
     void Update()
     {
-        // if (Input.GetMouseButtonDown(0))
-        // {
-        //     isDrawing = true;
-        // }
-        // else if (Input.GetMouseButtonUp(0))
-        // {
-        //     isDrawing = false;
-        // }
-
         if ((isDrawing) && (ammo >= 0))
         {
-            // DrawServerRpc(Input.mousePosition);
-            DrawServerRpc();
+            // Draw around cursor position on targetImage
+            DrawAroundCursor();
         }
     }
 
@@ -116,38 +107,26 @@ public class ImageDrawer : NetworkBehaviour
         }
     }
 
-
-    [ServerRpc]
-    private void DrawServerRpc()
+    void DrawAroundCursor()
     {
-        if (IsServer)
-        {
-            // Convert world position to screen position
-            Vector2 screenPosition2 = Camera.main.WorldToScreenPoint(followCursor.inputDirection);
+        // Convert cursor position to screen position
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(followCursor.inputDirection);
 
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(targetImage.rectTransform, screenPosition2, Camera.main, out Vector2 localPos);
+        // Convert screen position to local position in targetImage's RectTransform
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(targetImage.rectTransform, screenPosition, Camera.main, out Vector2 localPos);
 
-            // Map local position to texture coordinates
-            Rect rect = targetImage.rectTransform.rect;
-            float pivotOffsetX = rect.width * targetImage.rectTransform.pivot.x;
-            float pivotOffsetY = rect.height * targetImage.rectTransform.pivot.y;
-            float x = (localPos.x + pivotOffsetX) / rect.width * drawingTexture.width;
-            float y = (localPos.y + pivotOffsetY) / rect.height * drawingTexture.height;
+        // Calculate texture coordinates
+        Rect rect = targetImage.rectTransform.rect;
+        float pivotOffsetX = rect.width * targetImage.rectTransform.pivot.x;
+        float pivotOffsetY = rect.height * targetImage.rectTransform.pivot.y;
+        float x = (localPos.x + pivotOffsetX) / rect.width * drawingTexture.width;
+        float y = (localPos.y + pivotOffsetY) / rect.height * drawingTexture.height;
 
-            // Vector2 localPos;
-            // RectTransformUtility.ScreenPointToLocalPointInRectangle(targetImage.rectTransform, screenPosition, null, out localPos);
-
-            // Map local position to texture coordinates
-            // Rect rect = targetImage.rectTransform.rect;
-            // float x = (localPos.x - rect.x) / rect.width * drawingTexture.width;
-            // float y = (localPos.y - rect.y) / rect.height * drawingTexture.height;
-
-
-            DrawClientRpc(x, y);
-        }
+        // Draw on texture
+        DrawOnTexture((int)x, (int)y);
     }
-    [ClientRpc]
-    private void DrawClientRpc(float x, float y)
+
+    void DrawOnTexture(int centerX, int centerY)
     {
         for (int i = -brushSize; i <= brushSize; i++)
         {
@@ -155,8 +134,8 @@ public class ImageDrawer : NetworkBehaviour
             {
                 if (i * i + j * j <= brushSize * brushSize)
                 {
-                    int pixelX = Mathf.Clamp((int)x + i, 0, drawingTexture.width - 1);
-                    int pixelY = Mathf.Clamp((int)y + j, 0, drawingTexture.height - 1);
+                    int pixelX = Mathf.Clamp(centerX + i, 0, drawingTexture.width - 1);
+                    int pixelY = Mathf.Clamp(centerY + j, 0, drawingTexture.height - 1);
 
                     Color currentColor = drawingTexture.GetPixel(pixelX, pixelY);
 
@@ -166,14 +145,8 @@ public class ImageDrawer : NetworkBehaviour
                         drawingTexture.SetPixel(pixelX, pixelY, brushColor);
                         ammo--;
                         ShrinkStars();
-                        //Debug.Log("drawing");
-                    }
-                    else
-                    {
-                        //Debug.Log("already black");
                     }
                 }
-                    
             }
         }
 
